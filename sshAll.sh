659b8095ca47f7095632bae66cf7cf204bdddbde
Ave-Wat <wattsa2@carleton.edu>
2021-09-02 14:14:34 -0500

    finished the script - need to correct machine lists

--- sshAll.sh
+++ sshAll.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
-#run script as root
-#need flash drive plugged in
+
+#pass in parameters such that: bash sshAll.sh -c [file/folder to copy] -s [script to run]
+
+while getopts c:s: flag
+do
+	case "${flag}" in
+		c) copyPath=${OPTARG};;
+		s) scriptPath=${OPTARG};;
+	esac
+done
+
+echo $copyPath
+echo $scriptPath
 
 function makeConnection () {
 	hostname=$1
-	#scp -r /Volumes/LEGACYBOOT/Imaging2021/WindowsBootApp/RemoteWindowsBootAppInstaller/InstallPackage mathc        sadmin@$hostname.mathcs.carleton.edu:~
-	ssh mathcsadmin@$hostname.mathcs.carleton.edu #sudo -S bash WindowsBootAppInstaller.sh  
+
+	if ! [ -z "$copyPath" ]
+	then
+		scp -r $copyPath mathcsadmin@$hostname.mathcs.carleton.edu:~ 
+	fi
+	
+	if ! [ -z "$scriptPath" ]
+	then
+		ssh mathcsadmin@$hostname.mathcs.carleton.edu sudo -S bash $scriptPath
+	else
+		ssh mathcsadmin@$hostname.mathcs.carleton.edu
+	fi  
 }
+
 
 #roomNames=("olin208" "olin302" "olin304" "olin308" "olin310" "olin312")
 #numOfComputers=(3 3 9 10 24 7)
-roomNames=("olin302" "olin310")
-numOfComputers=(3 2)
+
+#these are shortened lists for testing
+roomNames=("olin308")
+numOfComputers=(2)
 
 for j in ${!roomNames[@]};
 do
